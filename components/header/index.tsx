d038fec35c0add9e88f7eb3c07903fdcad525ff9
ngrwisnu <adiprayogongrwisnu@gmail.com>
2022-12-10 10:36:41 +0700

    create hamburger menu component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
+import HamburgerMenu from "../ui/hamburger-menu";
+
 const Header = () => {
   return (
-    <div className="w-screen h-8">
-      <div className="h-full container-content bg-slate-500">
-        <p>Items</p>
+    <div className="w-screen">
+      <div className="container-content bg-stone-800">
+        <div className="flex items-center justify-between py-5 text-stone-50">
+          <Link href="/">
+            <span>NGR</span>
+          </Link>
+
+          <div className="navigation flex items-center">
+            <nav>
+              <ul className="flex">
+                <li>Project</li>
+                <li>About</li>
+              </ul>
+            </nav>
+            <HamburgerMenu />
+          </div>
+        </div>
       </div>
     </div>
   );
