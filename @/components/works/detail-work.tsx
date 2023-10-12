acc513b3e78eefab9bc7aca275e6baa7ba86c123
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-10-12 18:44:56 +0700

    [refactor] split component in detail project page (#5)

---
+++ @/components/works/detail-work.tsx
@@ -0,0 +1,121 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { Badge } from "../ui/badge";
+
+interface ProjectJumbotronProps {
+  title: string;
+  image: string;
+  webLink?: string;
+}
+
+interface ProjectDescriptionProps {
+  description: string;
+  designedBy: string;
+  role: string;
+  techs: string[];
+  details?: React.ReactNode;
+}
+
+const ProjectJumbotron = ({ title, image, webLink }: ProjectJumbotronProps) => {
+  return (
+    <div className="flex flex-col items-center gap-12">
+      <h2 className="text-3xl font-medium">{title}</h2>
+      {webLink && (
+        <Button asChild>
+          <Link href={webLink}>Visit Site</Link>
+        </Button>
+      )}
+      <figure className="flex max-h-[620px] w-full justify-center bg-[#F5F5F5]">
+        <Image
+          src={`/assets/images/${image}`}
+          width={1184}
+          height={620}
+          alt="Jumbotron"
+        />
+      </figure>
+    </div>
+  );
+};
+
+const ProjectDescription = ({
+  description,
+  designedBy,
+  role,
+  techs,
+  details,
+}: ProjectDescriptionProps) => {
+  return (
+    <div className="flex w-full flex-wrap gap-8 md:gap-0">
+      <div className="order-2 basis-full md:order-1 md:basis-2/3">
+        <div className="detail__description mb-8">
+          <h5>
+            <strong>Description</strong>
+          </h5>
+          <p className="mt-1 dark:text-stone-300 md:w-2/3">{description}</p>
+        </div>
+        <div className="detail__more-info flex">
+          <div className="more-info__designed-by basis-1/2 md:basis-1/3">
+            <h5>
+              <strong>Designed By</strong>
+            </h5>
+            <p className="mt-1 dark:text-stone-300">{designedBy}</p>
+          </div>
+          <div className="more-info__role basis-1/2 md:basis-1/3">
+            <h5>
+              <strong>Role</strong>
+            </h5>
+            <p className="mt-1 dark:text-stone-300">{role}</p>
+          </div>
+        </div>
+        {details && <div className="mt-8">{details}</div>}
+      </div>
+      <div className="order-1 basis-full md:order-2 md:basis-1/3">
+        <h5>
+          <strong>Build with</strong>
+        </h5>
+        <div className="mt-1 flex w-full flex-wrap gap-1">
+          {techs.map((tech: string) => {
+            return (
+              <Badge key={tech} variant="outline">
+                {tech}
+              </Badge>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ProjectGallery = ({ images }: { images: string[] | [] }) => {
+  return (
+    <ul className="flex flex-col gap-24">
+      {images.map((image) => (
+        <li
+          key={image}
+          className="flex max-h-[620px] w-full min-w-[360px] justify-center bg-[#F5F5F5]"
+        >
+          <Image
+            src={`/assets/images/${image}`}
+            width={1184}
+            height={620}
+            alt="gallery"
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ProjectDetail = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section id="project-detail" className="w-full px-4 dark:text-white">
+      <div className="container-content py-24">
+        <div className="flex flex-col gap-24">{children}</div>
+      </div>
+    </section>
+  );
+};
+
+export { ProjectDetail, ProjectJumbotron, ProjectDescription, ProjectGallery };
--- app/works/[slug]/page.tsx
+++ app/works/[slug]/page.tsx
@@ -1,104 +1,39 @@
 import React from "react";
 import data from "../../../data/works";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { Badge } from "@/components/ui/badge";
+import {
+  ProjectDescription,
+  ProjectDetail,
+  ProjectGallery,
+  ProjectJumbotron,
+} from "@/components/works/detail-work";
 
 const Work = ({ params }: { params: { slug: string } }) => {
+  const item = data.find((obj) => obj.slug === params.slug);
+
   return (
-    <section id="project-detail" className="w-full dark:text-white max-md:px-4">
-      <div className="container-content py-24">
-        {data.map((item) => {
-          if (item.slug === params.slug) {
-            return (
-              <div className="flex flex-col gap-24" key={item.slug}>
-                {/* Hero section */}
-                <div className="flex flex-col items-center gap-12">
-                  <h2 className="text-3xl font-medium">{item.title}</h2>
-                  <Button asChild>
-                    <Link href={item.linkExternal}>Visit Site</Link>
-                  </Button>
-                  <figure className="flex max-h-[620px] w-full justify-center bg-[#F5F5F5]">
-                    <Image
-                      src={`/assets/images/${item.imgUrls[1]}`}
-                      width={1184}
-                      height={620}
-                      alt="Jumbotron"
-                    />
-                  </figure>
-                </div>
+    <ProjectDetail>
+      {item && (
+        <>
+          {/* Jumbotron section */}
+          <ProjectJumbotron
+            title={item.title}
+            image={item.imgUrls[1]}
+            webLink={item.linkExternal}
+          />
 
-                {/* Description */}
-                <div className="flex w-full flex-wrap gap-8 md:gap-0">
-                  <div className="basis-full md:basis-2/3">
-                    <div className="detail__description mb-8">
-                      <h5>
-                        <strong>Description</strong>
-                      </h5>
-                      <p className="mt-1 dark:text-stone-300 md:w-2/3">
-                        {item.description}
-                      </p>
-                    </div>
-                    <div className="detail__more-info flex">
-                      <div className="more-info__designed-by basis-1/2 md:basis-1/3">
-                        <h5>
-                          <strong>Designed By</strong>
-                        </h5>
-                        <p className="mt-1 dark:text-stone-300">
-                          {item.designedBy}
-                        </p>
-                      </div>
-                      <div className="more-info__role basis-1/2 md:basis-1/3">
-                        <h5>
-                          <strong>Role</strong>
-                        </h5>
-                        <p className="mt-1 dark:text-stone-300">
-                          {item.myRole}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="basis-full md:basis-1/3">
-                    <h5>
-                      <strong>Build with</strong>
-                    </h5>
-                    <div className="mt-1 flex w-full flex-wrap gap-1">
-                      {item.technologies.map((tech: string) => {
-                        return (
-                          <Badge key={tech} variant="outline">
-                            {tech}
-                          </Badge>
-                        );
-                      })}
-                    </div>
-                  </div>
-                </div>
+          {/* Description */}
+          <ProjectDescription
+            description={item.description}
+            designedBy={item.designedBy}
+            role={item.myRole}
+            techs={item.technologies}
+          />
 
-                {/* Image gallery */}
-                <ul className="flex flex-col gap-24">
-                  {item.imgUrls.slice(2).map((image) => {
-                    return (
-                      <li
-                        key={image}
-                        className="flex max-h-[620px] w-full min-w-[360px] justify-center bg-[#F5F5F5]"
-                      >
-                        <Image
-                          src={`/assets/images/${image}`}
-                          width={1184}
-                          height={620}
-                          alt="gallery"
-                        />
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            );
-          }
-        })}
-      </div>
-    </section>
+          {/* Image gallery */}
+          <ProjectGallery images={item.imgUrls.slice(2)} />
+        </>
+      )}
+    </ProjectDetail>
   );
 };
 
